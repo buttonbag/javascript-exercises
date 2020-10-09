6a62eb16795974dbf2cc6a7581f12b46daecd72c
I.T Loaner <louiecamacho@gmail.com>
2020-10-09 14:27:41 -0500

    repeatString passes 7 tests

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (text, num) {
+	if (num <= -1) {
+		return 'ERROR';
+	}
+	let words = '';
+	for (let i = 0; i < num; i++) {
+		words += text;
+	}
+	return words;
+};
 
-}
-
-module.exports = repeatString
+module.exports = repeatString;
