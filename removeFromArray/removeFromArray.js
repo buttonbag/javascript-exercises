e5c61baa126a01305f67babb4fa3aebb5ccbde5b
I.T Loaner <louiecamacho@gmail.com>
2020-10-18 10:20:13 -0500

    spreads arguments

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = (...a) => {
+	let myArray = a[0];
 
-}
+	for (i = 1; i < a.length; i++) {
+		if (
+			/* value not present */ typeof a[i] !== 'undefined'
+		) {
+			/* ignore */
+			console.log(a[i]);
+		} else {
+			myArray.splice(myArray.indexOf(a[i]), 1);
+		}
+		return myArray;
+	}
+};
 
-module.exports = removeFromArray
+module.exports = removeFromArray;
